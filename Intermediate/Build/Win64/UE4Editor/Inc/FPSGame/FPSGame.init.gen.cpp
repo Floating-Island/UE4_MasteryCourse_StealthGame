5e07713b416845c7edae62097a0b57b5f2000f09
Floating-Island <betomiku@gmail.com>
2019-09-11 17:21:53 -0300

    FPSObjectiveActor class created.

--- Intermediate/Build/Win64/UE4Editor/Inc/FPSGame/FPSGame.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/FPSGame/FPSGame.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeFPSGame_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x60ED6A54,
-				0x7C751294,
+				0x7D7250C8,
+				0x3361A901,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
