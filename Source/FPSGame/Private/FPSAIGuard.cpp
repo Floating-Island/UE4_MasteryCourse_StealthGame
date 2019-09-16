5f5f32b157553e2e9cc242e288aa0830b26aec6c
Floating-Island <betomiku@gmail.com>
2019-09-16 00:32:34 -0300

    added skeleton via blueprint. Added sensing component.

--- Source/FPSGame/Private/FPSAIGuard.cpp
+++ Source/FPSGame/Private/FPSAIGuard.cpp
@@ -2,6 +2,7 @@
 
 
 #include "FPSAIGuard.h"
+#include "Perception\PawnSensingComponent.h"
 
 // Sets default values
 AFPSAIGuard::AFPSAIGuard()
@@ -9,6 +10,7 @@ AFPSAIGuard::AFPSAIGuard()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	sensingComponent = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("Sensing Component"));//doesn't need an attachment, it's not a scene component.
 }
 
 // Called when the game starts or when spawned
--- Source/FPSGame/Public/FPSAIGuard.h
+++ Source/FPSGame/Public/FPSAIGuard.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "FPSAIGuard.generated.h"
 
+class UPawnSensingComponent;
+
 UCLASS()
 class FPSGAME_API AFPSAIGuard : public ACharacter
 {
@@ -18,6 +20,9 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UPawnSensingComponent* sensingComponent;
 
 public:	
 	// Called every frame
