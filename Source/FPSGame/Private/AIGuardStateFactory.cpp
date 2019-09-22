a5142a1b3afd9807083b6274a0e6c7361a129f0d
Floating-Island <betomiku@gmail.com>
2019-09-21 23:17:28 -0300

    created a factory of AIGuardStates

---
+++ Source/FPSGame/Private/AIGuardStateFactory.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AIGuardStateFactory.h"
+#include "AIGuardIdleState.h"
+#include "AIGuardSuspiciousState.h"
+#include "AIGuardAlertedState.h"
+
+AIGuardStateFactory::AIGuardStateFactory()
+{
+	state = AIGuardIdleState::singletonInstance();
+}
+
+AIGuardState* AIGuardStateFactory::reactToNoise()
+{
+	return state->reactToNoise();
+}
+
+AIGuardState* AIGuardStateFactory::reactToSpotting()
+{
+	return state->reactToSpotting();
+}
+
+AIGuardState* AIGuardStateFactory::goingBackToOriginalPosition()
+{
+	return state->goingBackToOriginalPosition();
+}
+
---
+++ Source/FPSGame/Public/AIGuardStateFactory.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIGuardState.h"
+
+/**
+ * 
+ */
+class FPSGAME_API AIGuardStateFactory : public AIGuardState
+{
+private:
+	AIGuardState* state;
+public:
+	AIGuardStateFactory();
+	AIGuardState* reactToNoise();
+	AIGuardState* reactToSpotting();
+	AIGuardState* goingBackToOriginalPosition();
+
+};
